168262b5d95953a5c4ab12509b0f6f89b0485e7d
herney <herney400@gmail.com>
2012-10-01 10:25:44 -0500

    Ya estoy haciendo la clase estados para ver como los generamos.....compañero aunque creo que los movimientos ya estan diseñado en la clase robot

---
+++ src/estructuradeDatos/Cola.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuradeDatos;
+
+/**
+ *
+ * @author fidelhpc
+ */
+public class Cola {
+    
+}
