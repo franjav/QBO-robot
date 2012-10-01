8a43c53057448bce71b99e6525f0376e27c0c9e8
palacioscuacialpud <palacioscuacialpud@palacioscuacialpud-MS-7374>
2012-10-01 01:12:28 -0500

    He creado el objeto Nodo... por favor necesito me den mas ideas de lo que podria llevar esa clase.........

--- src/modelo/Nodo.java
+++ src/modelo/Nodo.java
@@ -16,20 +16,35 @@ public class Nodo {
      int peso;
      Robot robot; 
      int pesoBasura=0;
+     String ruta="";
+     private int heuristica; 
+     
+     /*en caso de que la heuristica la hagamos
+      * con entero, que creo es lo mas viable...
+     */
+     
      
      public Nodo(int mundoAmb[][], Point posicionActual, int peso){
       this.mundo=mundoAmb;
       this.posicionActual=posicionActual;
       this.peso=peso;
     }
-     
+    /*constructor 2*/ 
+    public Nodo() {
+    
+    }
+    public int getHeuristica() {
+        return heuristica;
+    }
+    public Point getPosicionActual() {
+        return posicionActual;
+    }
 
     public int getPesoBasura() {
         return pesoBasura;
     }
 
-    public Nodo() {
-    }
+    
 
     public int[][] getMundo() {
         return mundo;
