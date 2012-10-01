46a28de6b47afcb45533602269c6a33e84f60f8f
herney <herney400@gmail.com>
2012-10-01 11:02:36 -0500

    fr

--- src/estructuradeDatos/Cola.java
+++ src/estructuradeDatos/Cola.java
@@ -4,10 +4,28 @@
  */
 package estructuradeDatos;
 
+import java.util.Vector;
+import modelo.Nodo;
+
 /**
  *
  * @author fidelhpc
  */
 public class Cola {
+    
+    Vector vector=new Vector();
+    
+    
+    public void remover(Nodo nodo){
+      vector.remove(nodo);         
+    }
+    
+    
+    public void agregar(Nodo nodo){
+       vector.add(nodo);
+    }
+    
+    
+    
     
 }
