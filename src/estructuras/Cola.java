b9d806ceb29aa0c144657fc3803835b2a08d5d70
Franjav <Franjav@10.39.37.126>
2012-10-01 23:24:31 -0500

    Se adiciona la clase CargarMundo.

--- src/estructuras/Cola.java
+++ src/estructuras/Cola.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package estructuradeDatos;
+package estructuras;
 
 import java.util.Vector;
 import modelo.Nodo;
